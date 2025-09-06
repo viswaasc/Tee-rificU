f3907147882f17add7d18578139506621ccfd735
viswaasc <vwschndrn@gmail.com>
2025-09-06 20:15:33 +0530

    Update vite.config.js base path and deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
+import { resolve } from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Tee-rificU/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),  // 👈 Optional: convenient alias for imports
+      "@": resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    host: true,
+    port: 3000,
+  }
 })
